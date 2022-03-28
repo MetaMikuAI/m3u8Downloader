1c0fe2239d21dab7e46da9823e1199c15b6a96f1
【MATLAB】absent <46769316+MetaMikuAI@users.noreply.github.com>
2022-03-28 23:45:42 +0800

    Delete paste.py

--- paste.py
+++
@@ -1,9 +0,0 @@
-import ffmpy
-screen=input('视频>>>')
-sound=input('音频>>>')
-ff = ffmpy.FFmpeg(
-     inputs={screen:'-y',sound:None},
-     outputs={'output.mp4': ['-vcodec','copy',
-'-acodec','copy']})
-ff.run()
-print(ff.cmd)
